98271e7c3ca2077d548596a9cb2cf294a941c876
kapj1coh <tima.mtl@gmail.com>
2026-03-03 11:44:47 -0500

    noctalia and fix nvim shift arrow bug

--- wezterm.lua
+++ wezterm.lua
@@ -11,8 +11,8 @@ config.default_prog = { "nu", "-l" }
 
 -- Quick launch menu so you can pick shells from the window menu
 config.launch_menu = {
-  { label = "Nushell (login)", args = { "nu", "-l" } },
-  { label = "Bash (login)",    args = { "/usr/bin/bash", "-l" } },
+    { label = "Nushell (login)", args = { "nu", "-l" } },
+    { label = "Bash (login)",    args = { "/usr/bin/bash", "-l" } },
 }
 
 config.window_background_opacity = 90
@@ -25,18 +25,20 @@ config.initial_rows = 28
 
 -- Fonts
 config.font = wezterm.font_with_fallback({
-  "JetBrainsMono Nerd Font",
-  "FiraCode Nerd Font",
-  "Noto Color Emoji",
+    "JetBrainsMono Nerd Font",
+    "FiraCode Nerd Font",
+    "Noto Color Emoji",
 })
 config.font_size = 12
 
 -- Colors & transparency
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = 'Noctalia'
 config.window_background_opacity = 0.95
 config.text_background_opacity = 0.8
 config.window_close_confirmation = 'NeverPrompt'
+
 config.max_fps = 120
+config.scrollback_lines = 10000
 
 
 config.audible_bell = "Disabled"
@@ -50,30 +52,30 @@ config.enable_csi_u_key_encoding = true
 -- Keybindings
 config.keys = {
     -- Copy/Paste (no Shift circus)
-    { key = "C", mods = "CTRL", action = act.CopyTo("Clipboard") },
-    { key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+    { key = "C",          mods = "CTRL",       action = act.CopyTo("Clipboard") },
+    { key = "V",          mods = "CTRL",       action = act.PasteFrom("Clipboard") },
 
     -- Fullscreen
-    { key = "F11", mods = "NONE", action = act.ToggleFullScreen },
+    { key = "F11",        mods = "NONE",       action = act.ToggleFullScreen },
 
     -- Splits
-    { key = "D", mods = "CTRL|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { key = "D", mods = "CTRL|ALT",   action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "D",          mods = "CTRL|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "D",          mods = "CTRL|ALT",   action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
     -- Tabs
-    { key = "T", mods = "CTRL|SHIFT", action = act.SpawnTab("CurrentPaneDomain") },
-    { key = "N", mods = "CTRL|SHIFT", action = act.SpawnWindow },
+    { key = "T",          mods = "CTRL|SHIFT", action = act.SpawnTab("CurrentPaneDomain") },
+    { key = "N",          mods = "CTRL|SHIFT", action = act.SpawnWindow },
 
     -- Tab switching with Ctrl+Arrow
-    { key = "RightArrow", mods = "CTRL", action = act.ActivateTabRelative(1) },
-    { key = "LeftArrow",  mods = "CTRL", action = act.ActivateTabRelative(-1) },
+    { key = "RightArrow", mods = "CTRL",       action = act.ActivateTabRelative(1) },
+    { key = "LeftArrow",  mods = "CTRL",       action = act.ActivateTabRelative(-1) },
 
     -- Quick spawns
-    { key = "U", mods = "CTRL|SHIFT", action = act.SpawnCommandInNewTab({ args = { "nu", "-l" } }) },
-    { key = "B", mods = "CTRL|SHIFT", action = act.SpawnCommandInNewTab({ args = { "/usr/bin/bash", "-l" } }) },
+    { key = "U",          mods = "CTRL|SHIFT", action = act.SpawnCommandInNewTab({ args = { "nu", "-l" } }) },
+    { key = "B",          mods = "CTRL|SHIFT", action = act.SpawnCommandInNewTab({ args = { "/usr/bin/bash", "-l" } }) },
 
     -- Launcher for when you forget the shortcuts
-    { key = "L", mods = "CTRL|SHIFT", action = act.ShowLauncherArgs({ flags = "LAUNCH_MENU_ITEMS" }) },
+    { key = "L",          mods = "CTRL|SHIFT", action = act.ShowLauncherArgs({ flags = "LAUNCH_MENU_ITEMS" }) },
 
     -- vim specific
     {
@@ -87,9 +89,25 @@ config.keys = {
         action = wezterm.action.DisableDefaultAssignment,
     },
 
+    -- scrolling
+    { key = 'UpArrow',   mods = 'CTRL|SHIFT', action = act.ScrollByLine(-1) },
+    { key = 'DownArrow', mods = 'CTRL|SHIFT', action = act.ScrollByLine(1) },
+
 
 
 }
 
-return config
+config.mouse_bindings = {
+    {
+        event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+        mods = 'NONE',
+        action = act.ScrollByLine(-5),
+    },
+    {
+        event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+        mods = 'NONE',
+        action = act.ScrollByLine(5),
+    },
+}
 
+return config
